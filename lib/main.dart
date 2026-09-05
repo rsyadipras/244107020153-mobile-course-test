74271edd9d34445acea5a57b78ace8595644032b
Risky Adi Prasetya <rsyadipras@gmail.com>
2026-09-05 13:25:32 +0700

    feat: create week 1 Flutter profile app

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Profil Mahasiswa')),
+        body: const Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.verified, size: 72), // perubahan icon
+              SizedBox(height: 16),
+              Text('Risky Adi Prasetya', style: TextStyle(fontSize: 24)), // perubahan nama
+              Text('Pemrograman Mobile — Minggu 1'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
